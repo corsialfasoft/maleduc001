a1aac186efd3fdd1a32db9d52cc28101119bde05
JokerJackassJam <edo.bevione@gmail.com>
2018-04-11 11:51:48 +0200

    Creata Interface e DTO

--- Maled001/Models/DTO.cs
+++ Maled001/Models/DTO.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using System;
+
+namespace Maled001 {
+    public class Prodotto {
+        public int Codice {get; set;}
+        public string Descrizione {get; set;}
+        public int Quantita {get; set;}
+    }
+}
--- Maled001/Models/interface.cs
+++ Maled001/Models/interface.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System;
+using Maled001;
+using System.Collections.Generic;
+
+namespace Maled001 {
+    public interface IMaled {
+        Prodotto SearchByCode (int codice);
+        List<Prodotto> SearchByDescrizione (string descrizione);
+        void AddRequest (List<Prodotto> list);
+    }
+}
