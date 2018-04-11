0432cafc710c3911b553634f10ad24e038ecb1e3
mtani93 <matteo.cecina@gmail.com>
2018-04-11 12:27:28 +0200

    update

--- Maled001/Models/DomainModel.cs
+++ Maled001/Models/DomainModel.cs
@@ -1,15 +1,29 @@
 ﻿using System.Collections.Generic;
 using Maled001;
-public class Maled : IMaled {
-	void IMaled.AddRequest(List<Prodotto> list) {
-		throw new System.NotImplementedException();
-	}
+using System.Linq;
+namespace Maled001{
+	public class Maled : IMaled {
+		RICHIESTEEntities DB;
+		public void AddRequest(List<Prodotto> list) {
+			throw new System.NotImplementedException();
+		}
 
-	Prodotto IMaled.SearchByCode(int codice) {
-		throw new System.NotImplementedException();
-	}
+		public Prodotto SearchByCode(int codice) {
+			throw new System.NotImplementedException();
+		}
 
-	List<Prodotto> IMaled.SearchByDescrizione(string descrizione) {
-		throw new System.NotImplementedException();
+		public List<Prodotto> SearchByDescrizione(string descrizione) {
+			List<Prodotto> output = new List<Prodotto>();
+			using(DB = new RICHIESTEEntities()){
+				IEnumerable<ProdottiSet> query = from p in DB.ProdottiSet where p.descrizione.Contains(descrizione) select p;
+				foreach(ProdottiSet prod in  query){
+					Prodotto tmp = new Prodotto();
+					tmp.Codice = prod.Id;
+					tmp.Descrizione = prod.descrizione;
+					output.Add(tmp);
+				}
+			}
+			return output; 
+		}
 	}
 }
