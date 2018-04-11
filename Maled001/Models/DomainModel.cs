9da648db9d89d811cadfab583b09fd9253280e1f
mtani93 <matteo.cecina@gmail.com>
2018-04-11 11:56:50 +0200

    impl mok interfaccia

--- Maled001/Models/DomainModel.cs
+++ Maled001/Models/DomainModel.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System.Collections.Generic;
+using Maled001;
+public class Maled : IMaled {
+	void IMaled.AddRequest(List<Prodotto> list) {
+		throw new System.NotImplementedException();
+	}
+
+	Prodotto IMaled.SearchByCode(int codice) {
+		throw new System.NotImplementedException();
+	}
+
+	List<Prodotto> IMaled.SearchByDescrizione(string descrizione) {
+		throw new System.NotImplementedException();
+	}
+}
